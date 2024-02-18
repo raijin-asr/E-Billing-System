8e577164711e08fe2fddaa5333971f75749f5bdd
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-02-18 14:04:20 +0545

    E-billing system project upload

---
+++ E-Billing System/.metadata/.plugins/org.eclipse.core.resources/.history/9d/70deda79bf2f001e17dff453933ff5f2
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author GPT4
+ *
+ */
+module billPrj {
+}
---
+++ E-Billing System/billPrj/src/billPrj/Conn.java
@@ -0,0 +1,20 @@
+package billPrj;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///firstdb","root","root");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
